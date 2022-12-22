a6c429aee0ba9ad61203ef14347eeb298fe5242a
Ayan Ahmed <49174403+SodaisNasir@users.noreply.github.com>
2022-12-22 18:51:51 +0500

    Apis setted on client domain

--- constants/Url.js
+++ constants/Url.js
@@ -1,4 +1,4 @@
 // export const APIURL = 'https://todaysfresh.com.pk';
 // export const APIURL = 'https://sassolution.org/mart';
 // export const APIURL = 'https://mrmartindia.com/mart';
-export const APIURL = 'https://sassolution.org/pizza_blitz';
+export const APIURL = 'https://pizzablitzöstringen.de/pizza_blitz';
